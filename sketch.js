adc6e23c9538b8701692035ed57af79be12e9319
D-Erny <46407596+D-Erny@users.noreply.github.com>
2019-04-25 08:20:00 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
+var starbeams = [];
 function setup() {
   createCanvas(512,512);
+  for(var i = 0; i < 25; i++){
+    starbeams[i] = new particle([random(-25,25),random(-25,25)],[random(1,2),random(1,2)],1,[width,height],[255,255,25,255])
+  }
 }
 
 function draw() {
     background(10,25,175);
-    fill(255,255,255);
-    ellipse(mouseX,mouseY,100,100);
+    fill(255,255,25);
+    for(var i = 0; i < 25; i++){
+        starbeams[i].move()
+        starbeams[i].show()
+    }
+    
 }
